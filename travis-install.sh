8fb2f234dec1e95a64ba9026fcdd26e0c92a770d
Rhys Moyne <rhysmoyne@users.noreply.github.com>
2016-09-08 13:37:53 +0930

    Update travis-install.sh

--- travis-install.sh
+++ travis-install.sh
@@ -4,6 +4,8 @@ git submodule init && \
 git submodule update && \
 git submodule update --remote content/projects && \
 git add content/projects && \
+git config user.name "Rhys Moyne" && \
+git config user.email "rhys@creativekidssa.com.au" && \
 git commit -m "Update projects to latest content." && \
 git push -u origin master && \
 git status
