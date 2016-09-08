2b61747f4985268252928addd9aab8ab0d8c710b
Rhys Moyne <rhysmoyne@users.noreply.github.com>
2016-09-08 13:31:09 +0930

    Update travis-install.sh

--- travis-install.sh
+++ travis-install.sh
@@ -3,4 +3,7 @@ pip install lektor && \
 git submodule init && \
 git submodule update && \
 git submodule update --remote content/projects && \
+git add content/projects && \
+git commit -m "Update projects to latest content." && \
+git push -u origin master && \
 git status
