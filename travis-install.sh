701cdde015646a47bd295fcb4a8d68af0ff0e6cf
Rhys Moyne <rhysmoyne@users.noreply.github.com>
2016-09-10 01:41:29 +0930

    Update travis-install.sh

--- travis-install.sh
+++ travis-install.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 pip install lektor && \
 git submodule init && \
+git config user.name "Rhys Moyne" && \
+git config user.email "rhys@creativekidssa.com.au" && \
 git submodule update && \
 git submodule update --remote content/projects && \
 git submodule update --remote assets/wiki && \
+cd assets/wiki && \
+git submodule init && \
+git submodule update && \
+git submodule update --remote data/pages && \
+git add data/pages && \
+git commit -m "Update wiki to latest content."; \
+cd ../.. && \
 git add content/projects && \
 git add assets/wiki && \
-git config user.name "Rhys Moyne" && \
-git config user.email "rhys@creativekidssa.com.au" && \
+
 git pull && \
 git commit -m "Update projects and wiki to latest content."; \
 git push -u origin master && \
