c3a3580122fcff12339b868c83e311c0fbc4ae31
Rhys Moyne <rhys@creativekidssa.com.au>
2016-09-21 16:03:16 +0930

    Update to remove SWF dependency.

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 zip -r assets/static/creativekids.zip assets/ content/ models/ packages/ src/ templates/ README.md LICENSE build.sh clean.sh school-website.lektorproject screenshot.png travis-deploy.sh travis-install.sh
-cd src/scratch-flash
-./gradlew build -Ptarget=11.6
-cd ../..
-mkdir -p assets/static/swf
-cp src/scratch-flash/build/11.6/Scratch.swf assets/static/swf/Scratch.swf
-lektor build
+lektor build -O build/
